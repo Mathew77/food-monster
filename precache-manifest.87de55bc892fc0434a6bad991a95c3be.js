d32c368f30d22e5384595d40d91947abff845299
Mathew <mathewadegbite@gmail.com>
2020-01-17 17:56:56 +0100

    Updates

---
+++ precache-manifest.87de55bc892fc0434a6bad991a95c3be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a48263d2cc0d4efd5288ccead88616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5688f4bfb10912a2d2ae",
+    "url": "/static/css/main.2a57c172.chunk.css"
+  },
+  {
+    "revision": "22e44253a96300fa3f1b",
+    "url": "/static/js/2.ce141986.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ce141986.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5688f4bfb10912a2d2ae",
+    "url": "/static/js/main.43a9fc40.chunk.js"
+  },
+  {
+    "revision": "97befe85aa8ff0f2f27d",
+    "url": "/static/js/runtime-main.3da95384.js"
+  }
+]);
